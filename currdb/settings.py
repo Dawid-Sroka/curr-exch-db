ecc72089fdbc32502ea358dcdf2e988609b4e581
Dawid-Sroka <94313700+Dawid-Sroka@users.noreply.github.com>
2024-11-27 17:47:18 +0100

    Cleanup

--- currdb/settings.py
+++ currdb/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'currapp.apps.CurrappConfig'
+    'currapp.apps.CurrappConfig',
 	'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',

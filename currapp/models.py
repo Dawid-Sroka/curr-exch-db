ec329dacaa7b0e6e5d5b98b78a1b3da4bc3f2ab0
Dawid-Sroka <94313700+Dawid-Sroka@users.noreply.github.com>
2024-12-02 22:05:07 +0100

    Add str methods to models

--- currapp/models.py
+++ currapp/models.py
@@ -3,7 +3,13 @@ from django.db import models
 class Currency(models.Model):
     code = models.CharField(max_length=3)
 
+    def __str__(self):
+        return self.code
+
 
 class ExchangeRate(models.Model):
     currency_pair = models.CharField(max_length=6)
     exchange_rate = models.FloatField()
+
+    def __str__(self):
+        return f'{self.currency_pair}: {self.exchange_rate}'

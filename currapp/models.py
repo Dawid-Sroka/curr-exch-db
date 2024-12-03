74ddc73ca91b94e6e2d5dac6167773da45f14c45
Dawid-Sroka <94313700+Dawid-Sroka@users.noreply.github.com>
2024-12-03 19:53:05 +0100

    Change models

--- currapp/models.py
+++ currapp/models.py
@@ -7,9 +7,10 @@ class Currency(models.Model):
         return self.code
 
 
-class ExchangeRate(models.Model):
+class ExchangeRateAtDate(models.Model):
     currency_pair = models.CharField(max_length=6)
     exchange_rate = models.FloatField()
+    date = models.DateTimeField("date registered")
 
     def __str__(self):
-        return f'{self.currency_pair}: {self.exchange_rate}'
+        return f'{self.currency_pair}: {self.exchange_rate} at {self.date}'

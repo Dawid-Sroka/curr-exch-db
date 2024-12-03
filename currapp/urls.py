be0aa312181936530644f687ceadae53b3d7f6b6
Dawid-Sroka <94313700+Dawid-Sroka@users.noreply.github.com>
2024-12-03 22:43:34 +0100

    Add fetching data from yfinance

--- currapp/urls.py
+++ currapp/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("fetch", views.fetch, name="fetching"),
     path("", views.currency, name="currency"),
     path("<str:base_curr>/<str:arg_curr>/", views.currency_exchange, name="currency_exchange"),
 ]
--- currapp/views.py
+++ currapp/views.py
@@ -3,9 +3,27 @@ from django.http import HttpResponse
 
 from .models import Currency, ExchangeRateAtDate    
 import json
+from datetime import datetime
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+import yfinance as yf
+
+def fetch(request):
+    pairs = ["EURUSD", "USDJPY", "PLNUSD"]
+    for pair in pairs:
+        ticker = pair + "=X"
+
+        end_date = datetime.today().date()
+        start_date = "2024-11-25"
+
+        data = yf.download(ticker, start=start_date, end=end_date)
+
+        open_dict = data['Open'].dropna().to_dict()[ticker]
+        items = open_dict.items()
+        open_dict_formatted = {date.strftime('%Y-%m-%d'): rate for date, rate in items}
+        for k, v in open_dict_formatted.items():
+            e = ExchangeRateAtDate(currency_pair=pair, exchange_rate=v, date=k)
+            e.save()
+    return HttpResponse("Hello, data has been fetched.")
 
 def currency(request):
     output = Currency.objects.all()
@@ -13,5 +31,5 @@ def currency(request):
 
 def currency_exchange(request, base_curr, arg_curr):
     target_pairs = ExchangeRateAtDate.objects.filter(currency_pair = base_curr + arg_curr)
-    newest = target_pairs.order_by("date")[0]
+    newest = target_pairs.order_by("date").first()
     return HttpResponse(json.dumps(newest.get_value()))

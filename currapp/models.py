b463b0dd3119fc47225413054ed8820107d40d0b
Dawid-Sroka <94313700+Dawid-Sroka@users.noreply.github.com>
2024-12-03 22:43:15 +0100

    Format exchange rate value

--- currapp/models.py
+++ currapp/models.py
@@ -13,7 +13,7 @@ class ExchangeRateAtDate(models.Model):
     date = models.DateTimeField("date registered")
 
     def get_value(self):
-        return {"currency_pair": self.currency_pair, "exchange_rate": self.exchange_rate}
+        return {"currency_pair": self.currency_pair, "exchange_rate": f'{self.exchange_rate:.3f}'}
 
     def __str__(self):
-        return f'{self.currency_pair}: {self.exchange_rate} at {self.date}'
+        return f'{self.currency_pair}: {self.exchange_rate:.3f} at {self.date}' 

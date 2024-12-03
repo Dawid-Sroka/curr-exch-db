90ed226f80635711a51736e9975863c2bc8d0754
Dawid-Sroka <94313700+Dawid-Sroka@users.noreply.github.com>
2024-12-03 19:53:32 +0100

    Register models in admin

--- currapp/admin.py
+++ currapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Currency, ExchangeRateAtDate
+
+admin.site.register(Currency)
+admin.site.register(Currency, ExchangeRateAtDate)

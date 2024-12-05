5b86ef28064542fa4b591222396a744c9fd4b1f1
Dawid-Sroka <94313700+Dawid-Sroka@users.noreply.github.com>
2024-12-05 19:43:59 +0100

    Small fixes

--- currapp/views.py
+++ currapp/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import Currency, ExchangeRateAtDate    
+from .models import Currency, ExchangeRateAtDate
+from .load_data import load_data_from_yf, load_recent_data_from_yf
+
 import json
 from datetime import datetime
 
@@ -9,22 +11,7 @@ import yfinance as yf
 
 def fetch(request):
     currs = ["EUR", "USD", "PLN", "JPY"]
-    for curr in currs:
-        c = Currency(code=curr)
-        c.save()
-    pairs = [c1+c2 for c1 in currs for c2 in currs if c1 != c2]
-    for pair in pairs:
-        ticker = pair + "=X"
-
-        end_date = datetime.today().date()
-        start_date = "2024-11-26"
-
-        yf_data = yf.download(ticker, start=start_date, end=end_date)
-
-        data = yf_data['Open'].dropna().to_dict()[ticker].items()
-        open_dict_formatted = {date.strftime('%Y-%m-%d'): rate for date, rate in data}
-        for k, v in open_dict_formatted.items():
-            _ = ExchangeRateAtDate.objects.get_or_create(currency_pair=pair, exchange_rate=v, date=k)
+    load_recent_data_from_yf(currs)
     return HttpResponse("Hello, data has been fetched.")
 
 def currency(request):
@@ -33,5 +20,8 @@ def currency(request):
 
 def currency_exchange(request, base_curr, arg_curr):
     target_pairs = ExchangeRateAtDate.objects.filter(currency_pair = base_curr + arg_curr)
-    newest = target_pairs.order_by("-date").first()
-    return HttpResponse(json.dumps(newest.get_value()))
+    if target_pairs:
+        newest = target_pairs.order_by("-date").first().get_value()
+    else:
+        newest = {}
+    return HttpResponse(json.dumps(newest))

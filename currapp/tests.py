e2f426175d9d9f9faaa76191a4a3b25390bb9215
Dawid-Sroka <94313700+Dawid-Sroka@users.noreply.github.com>
2024-12-05 15:30:45 +0100

    Add first test

--- currapp/tests.py
+++ currapp/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Currency
+
+class CurrencyViewTests(TestCase):
+    def test_currency_endpoint_returns_all_currencies(self):
+        c1 = Currency.objects.create(code="PLN")
+        c2 = Currency.objects.create(code="USD")
+        c3 = Currency.objects.create(code="EUR")
+        expected_endpoint_response = [
+            {"code": "PLN"}, {"code": "USD"}, {"code": "EUR"}
+        ]
+        response = self.client.get(reverse("currency"))
+        self.assertEqual(response.status_code, 200)
+        self.assertJSONEqual(
+            str(response.content, encoding='utf8'),
+            expected_endpoint_response    
+        )
